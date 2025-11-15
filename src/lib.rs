96347646e7113ae6adcd3ed8ac78d8b6e18fc333
Joachim Lardinois <lardinois.joachim@gmail.com>
2025-11-15 17:56:02 +0100

    Prototype for type checking

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,121 @@
-pub fn add(x: usize, y: usize) -> usize {
-    x + y
+use std::fmt::Display;
+
+// TODO: Make it so that a single trick can return multiple errors.
+
+#[derive(Clone, Debug, Default)]
+pub struct Spell {
+    pub fragment: Fragment,
+    pub sub_spells: Vec<Spell>,
+}
+
+#[derive(Clone, Debug, Default)]
+pub enum Fragment {
+    #[default]
+    Void,
+    Number(f64),
+    Trick(Trick),
+}
+
+#[derive(Clone, Copy, Debug, Default, PartialEq, Eq)]
+pub enum FragmentType {
+    #[default]
+    Void,
+    Number,
+    Trick,
+}
+
+#[derive(Clone, Debug)]
+pub struct Trick {
+    pub evaluate_func: fn(&[Fragment]) -> Result<Fragment, String>,
+    pub type_checker: fn(&[FragmentType]) -> Option<String>,
+    pub output_type: FragmentType,
+}
+
+impl Spell {
+    pub fn execute(&self) -> Result<Fragment, String> {
+        let mut inputs: Vec<Fragment> = vec![];
+        for sub in &self.sub_spells {
+            inputs.push(sub.execute()?);
+        }
+        match &self.fragment {
+            Fragment::Void | Fragment::Number(_) => Ok(self.fragment.clone()),
+            Fragment::Trick(trick) => (trick.evaluate_func)(&inputs),
+        }
+    }
+    pub fn type_check(&self) -> Option<String> {
+        self.type_check_rec(vec![])
+    }
+    pub fn type_check_rec(&self, adress: Vec<usize>) -> Option<String> {
+        let mut errors: Vec<String> = self
+            .sub_spells
+            .iter()
+            .enumerate()
+            .filter_map(|(index, sub)| {
+                let mut new_adress = adress.clone();
+                new_adress.push(index);
+                sub.type_check_rec(new_adress)
+            })
+            .collect();
+        let Fragment::Trick(trick) = &self.fragment else {
+            if errors.is_empty() {
+                return None;
+            } else {
+                return Some(errors.join("\n"));
+            }
+        };
+        let inputs: Vec<FragmentType> = self
+            .sub_spells
+            .iter()
+            .map(|sub| sub.fragment.output_type())
+            .collect();
+        if let Some(new_error) = (trick.type_checker)(&inputs) {
+            errors.insert(0, format!("at {}: {}", adress_to_str(&adress), new_error));
+        }
+        if errors.is_empty() {
+            None
+        } else {
+            Some(errors.join("\n"))
+        }
+    }
+}
+
+impl From<Fragment> for FragmentType {
+    fn from(value: Fragment) -> Self {
+        match value {
+            Fragment::Void => FragmentType::Void,
+            Fragment::Number(_) => FragmentType::Number,
+            Fragment::Trick(_) => FragmentType::Trick,
+        }
+    }
+}
+
+impl Fragment {
+    pub fn output_type(&self) -> FragmentType {
+        match self {
+            Fragment::Trick(trick) => trick.output_type,
+            _ => self.clone().into(),
+        }
+    }
+}
+impl Display for FragmentType {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let a = match self {
+            FragmentType::Void => "Void",
+            FragmentType::Number => "Number",
+            FragmentType::Trick => "Trick",
+        };
+        f.pad(a)
+    }
+}
+
+fn adress_to_str(adress: &[usize]) -> String {
+    let a: Vec<String> = adress.iter().map(|ind| ind.to_string()).collect();
+    format!("[{}]", a.join(","))
 }
 
 #[cfg(test)]
 mod tests {
-    use crate::add;
 
     #[test]
-    fn testing() {
-        assert_eq!(4, add(1, 3));
-    }
+    fn testing() {}
 }
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,70 @@
+use trickster::{Fragment, FragmentType, Spell, Trick};
+
 fn main() {
-    println!("Hello, world!");
+    let a = to_spell("+2+2+").unwrap();
+    if let Some(error) = a.type_check() {
+        println!("Found {} errors:", error.split('\n').count());
+        println!("{}", error);
+    } else {
+        println!("No errors");
+    }
+    //dbg!(a.execute().unwrap());
+    //println!("Hello, world!");
+}
+
+fn to_spell(input: &str) -> Option<Spell> {
+    if let Ok(num) = input.trim().parse::<f64>() {
+        Some(Spell {
+            fragment: Fragment::Number(num),
+            sub_spells: vec![],
+        })
+    } else if input.trim().is_empty() {
+        Some(Spell {
+            fragment: Fragment::Void,
+            sub_spells: vec![],
+        })
+    } else {
+        let (lhs, rhs) = input.split_once('+')?;
+        Some(Spell {
+            fragment: Fragment::Trick(Trick {
+                evaluate_func: add,
+                type_checker: add_checker,
+                output_type: FragmentType::Number,
+            }),
+            sub_spells: vec![to_spell(lhs)?, to_spell(rhs)?],
+        })
+    }
+}
+
+fn add_checker(inputs: &[FragmentType]) -> Option<String> {
+    if inputs.len() < 2 {
+        return Some(format!(
+            "Wrong input count: Expected 2 or more inputs, got {}.",
+            inputs.len()
+        ));
+    }
+    for (index, other) in inputs.iter().enumerate() {
+        if *other != FragmentType::Number {
+            return Some(format!(
+                "Wrong input type: input {} is {} instead of Number.",
+                index + 1,
+                other
+            ));
+        }
+    }
+    None
+}
+fn add(inputs: &[Fragment]) -> Result<Fragment, String> {
+    if inputs.len() < 2 {
+        return Err("Tried to do addition with too few arguments.".to_string());
+    }
+    let mut total = 0.0;
+    for frag in inputs {
+        if let Fragment::Number(num) = frag {
+            total += num;
+        } else {
+            return Err("Tried to add something else than a number.".to_string());
+        }
+    }
+    Ok(Fragment::Number(total))
 }
